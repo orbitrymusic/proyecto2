18912a499862f336cea004b487d969ea356017ab
orbitrymusic <orbitrymusic@gmail.com>
2021-04-09 22:11:23 -0300

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './pages/cabecera/cabecera.component';
+import { CuerpoComponent } from './pages/cuerpo/cuerpo.component';
+import { PieComponent } from './pages/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    CuerpoComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
